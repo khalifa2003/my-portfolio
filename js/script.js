0445af9e99cd938c84baa926bdd18562854196b5
khalifa2003 <132180439+khalifa2003@users.noreply.github.com>
2024-09-25 13:01:40 +0300

    add some projects

--- js/script.js
+++ js/script.js
@@ -1,8 +1,26 @@
-// Select the hamburger button and the nav menu
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
-
-// When the hamburger is clicked
 hamburger.addEventListener("click", () => {
   navMenu.classList.toggle("active");
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const filterMenu = document.querySelectorAll(".filter-menu li");
+  const cards = document.querySelectorAll(".card");
+
+  filterMenu.forEach((menu) => {
+    menu.addEventListener("click", function () {
+      filterMenu.forEach((item) => item.classList.remove("active"));
+      this.classList.add("active");
+
+      const filterValue = this.getAttribute("data-filter");
+      cards.forEach((card) => {
+        if (filterValue === "all" || card.classList.contains(filterValue)) {
+          card.style.display = "block";
+        } else {
+          card.style.display = "none";
+        }
+      });
+    });
+  });
 });
