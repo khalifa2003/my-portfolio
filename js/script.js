fa559c2af5e99ebf790cb2e8af6dc7d07d6c9a69
khalifa2003 <132180439+khalifa2003@users.noreply.github.com>
2024-09-25 15:25:40 +0300

    finish soon

--- js/script.js
+++ js/script.js
@@ -24,3 +24,52 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
+function createSidebar() {
+  return `
+      <aside class="sidebar">
+        <img src="./assets/image.jpg" alt="Ahmad Khalifa" />
+        <h1>Ahmad Khalifa</h1>
+        <h3>Full Stack Developer</h3>
+
+        <!-- Contact Information -->
+        <div class="contact-info">
+          <p><i class="fas fa-envelope"></i> khalifa14112003@gmail.com</p>
+          <p><i class="fas fa-phone"></i> +20 1015388310</p>
+          <p><i class="fas fa-birthday-cake"></i> 14 November, 2003</p>
+          <p><i class="fas fa-map-marker-alt"></i> Alexandria, Egypt</p>
+        </div>
+
+        <!-- Social Media Icons -->
+        <div class="social-buttons">
+          <a href="https://www.facebook.com/khalifa112003" class="btn">
+            <div class="sign">
+              <i class="fab fa-facebook facebook"></i>
+            </div>
+            <div class="text">khalifa112003</div>
+          </a>
+
+          <a href="https://www.linkedin.com/in/khalifa2003/" class="btn">
+            <div class="sign">
+              <i class="fab fa-github github"></i>
+            </div>
+            <div class="text">@khalifa2003</div>
+          </a>
+
+          <a href="https://www.youtube.com/@ProgrammingArea" class="btn">
+            <div class="sign">
+              <i class="fab fa-youtube youtube"></i>
+            </div>
+            <div class="text">@ProgrammingArea</div>
+          </a>
+
+          <a href="https://github.com/khalifa2003" class="btn">
+            <div class="sign">
+              <i class="fab fa-linkedin linkedin"></i>
+            </div>
+            <div class="text">khalifa2003</div>
+          </a>
+        </div>
+      </aside>
+  `;
+}
+document.getElementById("sidebar").innerHTML = createSidebar();
